1acccb94b43fb06aae837c65568f87ea45c97f66
Mahamadou <mmohmo@yahoo.com>
2023-07-11 12:53:17 -0400

    created hamster class

---
+++ homework.js
@@ -0,0 +1,18 @@
+//* ================ Week 4 - Day 1 - HW ====================
+
+class Hamster {
+    constructor(owner="", name){
+        this.owner = owner
+        this.name = name
+        this.price = 15
+    }
+    wheelRun(){
+        console.log('squeak, squeak')
+    }
+    eatFood(){
+        console.log('nibble, nibble')
+    }
+    getPrice(){
+        return this.price
+    }
+}
